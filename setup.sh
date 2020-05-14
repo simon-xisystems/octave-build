0c5c0989a98603d59deb48f20c85ef361649245b
simon-xisystems <simon@xisystems.co.uk>
2020-05-14 20:15:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Pre-reqs
-sudo apt-get install build-essential git cmake libhdf5-dev libvtk5-dev libboost-all-dev libcgal-dev libtinyxml-dev libqt4-dev libvtk5-qt4-dev
+sudo apt-get install build-essential git cmake libhdf5-dev libvtk7-dev libboost-all-dev libcgal-dev libtinyxml-dev libqt4-dev libvtk7-qt-dev
 
 
 # Hyp to Mat 
@@ -18,7 +18,7 @@ sudo apt-get install paraview
 
 #Get Open EMS 
 
-wget http://openems.de/download/src/openEMS-v0.0.35.tar.bz2 -O /tmp
+wget http://openems.de/download/src/openEMS-v0.0.35.tar.bz2 -O /tmp/openEMS-v0.0.35.tar.bz2
 
 tar jxf /tmp/openEMS-v0.0.35.tar.bz2
 
