93c0bd260dd238bd05a24192a854d3aa82deae0a
simon-xisystems <simon@xisystems.co.uk>
2020-05-14 19:09:00 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Pre-reqs
+sudo apt-get install build-essential git cmake libhdf5-dev libvtk5-dev libboost-all-dev libcgal-dev libtinyxml-dev libqt4-dev libvtk5-qt4-dev
+
+
+# Hyp to Mat 
+sudo apt-get install gengetopt help2man groff pod2pdf bison flex libhpdf-dev libtool autoconf
+
+
+# Install octave and octave devel packages:
+
+sudo apt-get install octave liboctave-dev epstool transfig
+
+#Install ParaView so that gridded model can be viewed:
+
+sudo apt-get install paraview
+
+#Get Open EMS 
+
+wget http://openems.de/download/src/openEMS-v0.0.35.tar.bz2 -O /tmp
+
+tar jxf /tmp/openEMS-v0.0.35.tar.bz2
+
+sudo /tmp/update_openEMS.sh ~/opt/openEMS --with-hyp2mat --with-CTB
